9c78c1e9e8805a5efd6a22cf09ec4cb18a3837b9
Nguyễn Văn Vửng <130837811+VungNguyenYT@users.noreply.github.com>
2024-11-07 14:49:59 +0700

    up code
    navbar

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>GH Store</title>
 </head>
 <body>
@@ -10,18 +11,20 @@
         <div class="container-navbar">
             <div class="container-infor">
                 <div class="logo"></div>
-                <div class="search"></div>
+                <div class="search">
+                    <input type="text">
+                </div>
                 <div class="infor"></div>
             </div>
             <div class="container-pk">
-                <div class="pk-điong"></div>
+                <div class="pk-didong"></div>
                 <div class="pk-laptop"></div>
                 <div class="thietbimang"></div>
                 <div class="thietbi-luutru"></div>
                 <div class="pk-khac"></div>
             </div>
-
         </div>
+        <!-- BODY -->
         <div class="container-content">
             <div class="content"></div>
         </div>
